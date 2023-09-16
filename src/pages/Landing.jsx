83de2961f8a57a256a7055799329c2f508c68340
sarevir0 <sarevir0@gmail.com>
2023-09-16 10:23:32 +0200

    Section 4: Landing Page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,37 @@
+import styled from "styled-components";
+import Wrapper from "../assets/wrappers/LandingPage";
+import main from "../assets/images/main.svg";
+import { Link } from "react-router-dom";
+import Logo from "../components";
+
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Dignissimos reiciendis sunt asperiores quibusdam, aut, blanditiis
+            deserunt minima harum officia maiores enim, placeat aliquid tempore
+            nostrum itaque laborum excepturi eaque iste.
+          </p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn">
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
 };
+
 export default Landing;
