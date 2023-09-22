8ec137839abd15baecc29b8187316d41cd331a0c
sarevir0 <sarevir0@gmail.com>
2023-09-22 08:54:16 +0200

    Section 8:53. Dark Theme Logic - Bug Fix

--- src/pages/DashboardLayout.jsx
+++ src/pages/DashboardLayout.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Wrapper from "../assets/wrappers/Dashboard";
 import { BigSidebar, Navbar, SmallSidebar } from "../components";
 import { createContext, useContext, useState } from "react";
+import { checkDefaultTheme } from "../App";
 
 const DashboardContext = createContext();
 
@@ -9,10 +10,13 @@ const DashboardLayout = () => {
   // temp
   const user = { name: "john" };
   const [showSidebar, setShowSidebar] = useState(false);
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(checkDefaultTheme);
 
   const toggleDarkTheme = () => {
-    console.log("toggle dark theme");
+    const newDarkTheme = !isDarkTheme;
+    setIsDarkTheme(newDarkTheme);
+    document.body.classList.toggle("dark-theme", newDarkTheme);
+    localStorage.setItem("darkTheme", newDarkTheme);
   };
 
   const toggleSidebar = () => {
